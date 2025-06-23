d35dc783bc0cde9eda918842381372e33017257d
harshad <harshadrakshe28@gmail.com>
2025-06-23 14:52:35 +0530

    add

---
+++ 86 CommonJs Vs EcmaScript Modules/mymodule.js
@@ -0,0 +1,16 @@
+export const a = 1 //name export
+export const b = 2 //name export
+
+export const c = 3  //name export
+export const d = 4  //name export
+
+
+
+
+let obj;
+export default obj = {
+    x:5,
+    y:7
+}
+
+
