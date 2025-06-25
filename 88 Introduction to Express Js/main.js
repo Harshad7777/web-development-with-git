f9068071adaa08f573e589e9d6f169bf0a2cbc11
harshad <harshadrakshe28@gmail.com>
2025-06-25 12:19:25 +0530

    add

--- 88 Introduction to Express Js/main.js
+++ 88 Introduction to Express Js/main.js
@@ -16,15 +16,78 @@
 
 
 //express.js example hellowrld server
-const express = require('express')
+// const express = require('express')
+// const app = express()
+// const port = 3000
+//app.get or app.post or app.put or app.delete(path, handler)
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+// app.get('/about', (req, res) => {
+//   res.send('about us')
+// })
+// app.get('/contact', (req, res) => {
+//   res.send('Hello contact us')
+// })
+// app.get('/blog/intro to js', (req, res) => {
+//   //logic to get blog intro to js from database
+//   res.send('Hello blogs')
+// })
+// app.get('/blog/intro to python', (req, res) => {
+//   //logic to get blog intro to python from database
+//   res.send('Hello blogs python')
+// })
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+
+// parameter amd query 
+//logic to fetch {sluge} from database
+
+import express from 'express'
 const app = express()
 const port = 3000
 
-//app.get or app.post or app.put or app.delete(path, handler)
-app.get('/', (req, res) => {
+//static files
+app.use(express.static('public'))
+
+app.get('/', (_, res) => {
   res.send('Hello World!')
 })
+
+app.get('/about', (_, res) => {
+  res.send('about us')
+})
+
+app.get('/contact', (_, res) => {
+  res.send('Hello contact us')
+})
+
+app.get('/blog', (_, res) => {
+  res.send('Hello blog!')
+})
+
+app.get('/blog/:slug', (req, res) => {
+  //for url : http://localhost:3000/blog/intro-to-padosi?mode=dark&region=in
+  console.log(req.params) //will output {slug: 'intro to padosi'  }
+  console.log(req.query) //will output { mode: 'dark', region: 'in' }
+  res.send(`hello ${req.params.slug}`)
+})
+// app.get('/blog/intro to js', (req, res) => {
+//   //logic to get blog intro to js from database
+//   res.send('Hello blogs')
+// })
+// app.get('/blog/intro to python', (req, res) => {
+//   //logic to get blog intro to python from database
+//   res.send('Hello blogs python')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
