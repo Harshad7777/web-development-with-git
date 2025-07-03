10b839e9f074f0021b7b8a8ca4bd6c278fbb1abe
harshad <harshadrakshe28@gmail.com>
2025-07-03 12:24:58 +0530

    add

--- 99 Exercise 16/main.js
+++ 99 Exercise 16/main.js
@@ -2,35 +2,49 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const Employee = require("'./models/Employee");
-const { name } = require('ejs');
+const Employee = require("./models/Employee");
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/company');
+
+conn = mongoose.connect('mongodb://127.0.0.1:27017/company');
 const port = 3000
 
 
 
 app.set('view engine', 'ejs');
 
+const getRandom = (arr) => {
+    let rno = Math.floor(Math.random() * (arr.length - 1))
+    return arr[rno]
+}
+
 app.get('/', (req, res) => {
     res.render('index', { foo: 'FOO' });
 })
 app.get('/generate', async (req, res) => {
+    //clear the collection employees
+    await Employee.deleteMany({})
+
     //generate random data
+    let randomNames = ["soham", "harshad", "siddharth", "sachin", "pratik", "pranav", "siddharth", "siddharth", "siddharth", "siddharth"]
+    let randomLang = ["python", "java", "javascript", "c++", "c#", "php", "ruby", "go", "swift", "kotlin"]
+    let randomCities = ["Bilaspur", "muradabad", "Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Pune", "Hyderabad", "Jaipur"]
+
+
+
     for (let index = 0; index < 10; index++) {
-        let e = Employee.create({
-            name :"harshad",
-            salary : 3400000,
-            language: "JavaScript",
-            city: "Mumbai", 
-            isManager: true
+        let e = await Employee.create({
+            name: getRandom(randomNames),
+            salary: Math.floor(Math.random() * 100000),
+            language: getRandom(randomLang),
+            city: getRandom(randomCities),
+            isManager: (Math.random() > 0.5) ? true : false
 
         })
-        await e.save();
-      
-        
+        console.log(e);
+
+
     }
     res.render('index', { foo: 'FOO' });
 })
--- 99 Exercise 16/models/Employee.js
+++ 99 Exercise 16/models/Employee.js
@@ -1,9 +1,11 @@
-const employeeySchema = new mongoose.Schema({
-  name:String,
-  salary: number,
-  language: string,
-  city: string,
-  isManager:Boolean
-});
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  name: String,
+  salary: Number,
+  language: String,
+  city: String,
+  isManager: Boolean
+})
 const Employee = mongoose.model('Employee', employeeSchema);
-module.export = Employee
+module.exports = Employee
